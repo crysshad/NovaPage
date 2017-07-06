3f216cdac54a9f4c1fe8cdab038aa9d871cca396
Jing <Jing@10.0.0.142>
2017-07-05 22:44:18 -0400

    7_5_2017_Commit

---
+++ NovaPage/WebContent/js/NovaImageScript.js
@@ -0,0 +1,9 @@
+var app = angular.module("NovaApp", ["ngRoute"]);
+app.controller("NovaController", function($scope){
+			$scope.novaStuff ={
+			 novaHeadShot:"images/NovaHeadShot.png"
+			 
+				};
+    });  
+
+	
